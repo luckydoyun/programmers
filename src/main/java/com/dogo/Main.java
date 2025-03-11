b46cc395dd941f48b52d0ebf13fdfcdd67896720
luckydoyun <doyun.jang.0128@gamil.com>
2025-03-11 15:15:51 +0900

    inital commit

---
+++ src/main/java/com/dogo/Main.java
@@ -0,0 +1,16 @@
+package com.dogo;
+
+/**
+ * author       : jangdoyun
+ * date         : 25. 3. 11.
+ * description  :
+ * ===============================
+ * DATE     AUTHOR      NOTE
+ * -------------------------------
+ * 25. 3. 11.  jangdoyun     최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
