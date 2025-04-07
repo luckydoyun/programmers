177a9f2375a41bd0cc33b40269577d9641e31ce9
luckydoyun <doyun.jang.0128@gamil.com>
2025-04-07 09:20:40 +0900

    폰켓몬 문제풀이

--- src/main/java/com/dogo/Lessons1845.java
+++ src/main/java/com/dogo/Lessons1845.java
@@ -1,5 +1,9 @@
 package com.dogo;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  * author       : jangdoyun
  * date         : 25. 3. 11.
@@ -12,7 +16,7 @@ package com.dogo;
 public class Lessons1845 {
     public static void main(String[] args) {
 
-        int[] nums = {3,1,2,3};
+        int[] nums = {3, 1, 2, 3};
         System.out.println("정답:" + solution(nums));
     }
 
@@ -27,10 +31,28 @@ public class Lessons1845 {
      * nums의 길이(N)는 1 이상 10,000 이하의 자연수이며, 항상 짝수로 주어집니다.
      * 폰켓몬의 종류 번호는 1 이상 200,000 이하의 자연수로 나타냅니다.
      * 가장 많은 종류의 폰켓몬을 선택하는 방법이 여러 가지인 경우에도, 선택할 수 있는 폰켓몬 종류 개수의 최댓값 하나만 return 하면 됩니다.
+     *
      * @return
      */
     public static int solution(int[] nums) {
         int answer = 0;
+
+        // 선택할 수 있는 최대 마릿 수, nums는 항상 짝수 갯수이다.
+        int choice = nums.length / 2;
+
+        Map<String, Integer> choiceMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            choiceMap.put(String.valueOf(nums[i]), choiceMap.getOrDefault(String.valueOf(nums[i]), 0) + 1);
+
+            // 여러 종류의 폰켓몬을 고를 때 최대 마릿 수에 도달한 다면 멈추는 것이 성능에 좋다. (왜냐하면 종류 개수의 최댓값 하나만 return 하면 되므로)
+            if (choiceMap.size() == choice) {
+                break;
+            }
+        }
+
+        answer = choiceMap.size();
+
         return answer;
     }
 }
